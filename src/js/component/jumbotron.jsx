24b2055ca0ef8eacccd009c4b926937c05f24aca
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2023-11-06 19:31:37 +0000

    add navbar and jumbotron

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Junbotron = () => {
+    return (
+<div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">A Warm Welcome</h1>
+        <p class="col-md-8 fs-4">Lorem ipsum dolor sit amet. Et voluptate voluptate est voluptatem molestias eum animi totam ut earum assumenda sit veritatis galisum qui quia dolores? Est aspernatur iure et dolorum laborum vel corporis molestias qui exercitationem mollitia ea dolore nihil qui atque distinctio hic officia amet.</p>
+        <button class="btn btn-primary btn-lg" type="button">Call to action!</button>
+      </div>
+    </div>
+
+)
+}  
+export { Junbotron };
